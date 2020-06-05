c757df69c7e2a9812ca0c3522bff5e9a0d38acf5
karthikeyan rajendran <karthik_ricssion@yahoo.com>
2020-06-05 22:43:40 +0530

    added drag and drop feature

--- app.js
+++ app.js
@@ -18,22 +18,17 @@
         return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
-    var generateItemBoxObj = function() {
+    var generateItemBoxObj = function(type, x, y) {
         return {
-            type: 'div',
-            x: getRandomIntInclusive(0, dropZoneView.width - 100),
-            y: getRandomIntInclusive(0, dropZoneView.height - 50),
+            type: type || 'div',
+            x: x || getRandomIntInclusive(0, dropZoneView.width - 100),
+            y: y || getRandomIntInclusive(0, dropZoneView.height - 50),
         }
     };
 
-    
     var onMouseDown = function(e) {
         let elementId = (e.target || e.srcElement).id;
         let el = document.querySelector('#'+elementId)
-        // let getItems = document.querySelectorAll('.item')
-        // getItems.forEach((e) => {
-        //     e.classList.add('no-select') 
-        // })
 
         dropZoneView.selectedItem.el = el
         dropZoneView.selectedItem.mousedown = true; 
@@ -65,49 +60,84 @@
         dropZoneView.selectedItem.mousedown = false
     };
 
+    var addItemToDropZone = function(sNo, itemBox) {
+        let divElement = document.createElement('div')
+        divElement.className = 'item'
+        divElement.id = 'item-'+sNo
+        divElement.style.left = itemBox.x+'px';
+        divElement.style.top = itemBox.y+'px';
+        divElement.style.zIndex = sNo
+        divElement.innerText = sNo
+
+        divElement.addEventListener('mousedown', onMouseDown)
+        divElement.addEventListener('mouseup', onMouseUp)
+        divElement.addEventListener('mouseout', onMouseUp)
+        dropZoneElement.appendChild(divElement)
+    }
+
     var addInitialItemsToDropZoneByLength = function(len) {
         for(var i = 0; i < len; i++) {
             let itemBox = generateItemBoxObj()
             if(itemBox.type === 'div') {
                 let sNo = i + 1
-                let divElement = document.createElement('div')
-                divElement.className = 'item'
-                divElement.id = 'item-'+sNo
-                divElement.style.left = itemBox.x+'px';
-                divElement.style.top = itemBox.y+'px';
-                divElement.style.zIndex = sNo
-                divElement.innerText = sNo
-
-                divElement.addEventListener('mousedown', onMouseDown)
-                dropZoneElement.addEventListener('mousemove', onMouseMove)
-                divElement.addEventListener('mouseup', onMouseUp)
-                divElement.addEventListener('mouseout', onMouseUp)
-
-                dropZoneElement.appendChild(divElement)
+                addItemToDropZone(sNo, itemBox)
             }
         }
     };
 
-    // var onDrop = function(e) {
-    //     console.log('Dragging Drop')
-    // }
+    
+    var onDrop = function(e) {
+        e.preventDefault()
+        e.stopPropagation();
+        
+        console.log(e.clientX)
 
-    // var onDragEnter = function(e) {
-    //     console.log('Dragging Enter')
-    // }
+        let itemBox = generateItemBoxObj('div', (e.clientX - dropZoneElement.offsetLeft) , e.clientY)
+        if(itemBox.type === 'div') {            
+            let sNo = document.querySelectorAll('.item').length + 1
+            addItemToDropZone(sNo, itemBox)
+        }
+    }
 
-    // var onDragLeave = function(e) {
-    //     console.log('Dragging Leave')
-    // }
+    var onDragEnter = function(e) {
+        e.preventDefault()
+        e.stopPropagation();
 
-    // var onDragOver = function(e) {
-    //     console.log('Dragging over')
-    // }
+        console.log('Dragging Enter')
+    }
 
-    // dropZoneElement.addEventListener('drop', onDrop);
-    // dropZoneElement.addEventListener('dragenter', onDragEnter);
-    // dropZoneElement.addEventListener('dragleave', onDragLeave);
-    // dropZoneElement.addEventListener('dragover', onDragOver);
+    var onDragLeave = function(e) {
+        e.preventDefault()
+        e.stopPropagation();
+        console.log('Dragging Leave')
+    }
+
+    var onDragOver = function(e) {
+        e.preventDefault()
+        e.stopPropagation();
+        // console.log('Dragging over', e)
+    }
+
+    var onButtonEleDragStart = function(e) {
+        let overLayDiv = document.createElement('div')
+        overLayDiv.className = 'overlay-mask'
+        dropZoneElement.appendChild(overLayDiv)
+    }
+
+    var onButtonEleDragEnd = function(e) {
+        dropZoneElement.removeChild(document.querySelector('.overlay-mask'))
+    }
+
+    var buttonEle = document.querySelector('#buttonElement')
+    buttonEle.addEventListener('dragstart', onButtonEleDragStart)
+    buttonEle.addEventListener('dragend', onButtonEleDragEnd)
+
+    dropZoneElement.addEventListener('drop', onDrop);
+    dropZoneElement.addEventListener('dragenter', onDragEnter);
+    dropZoneElement.addEventListener('dragleave', onDragLeave);
+    dropZoneElement.addEventListener('dragover', onDragOver);
+
+    dropZoneElement.addEventListener('mousemove', onMouseMove)
     
     addInitialItemsToDropZoneByLength(10)
 
